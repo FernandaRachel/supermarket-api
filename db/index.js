c22ec3d3edbc0b5a355f0d92fd1355794d012c7f
Fernanda Tavares <fernanda.r.tavares@avanade.com>
2019-05-28 10:48:36 -0300

    log added

--- db/index.js
+++ db/index.js
@@ -3,6 +3,9 @@ const logger = require('../config/logger.config')();
 const URL_CONNECTION = process.env.MONGOLAB_URI &&
     process.env.NODE_ENV != 'development' ? process.env.MONGOLAB_URI : "mongodb://localhost/supermarket"
 
+logger.info(`MONGOLAB_URI: ${process.env.MONGOLAB_URI}`);
+logger.info(`NODE_ENV: ${process.env.NODE_ENV}`);
+
 module.exports = function() {
     mongoose
         .connect(URL_CONNECTION, { useNewUrlParser: true });
